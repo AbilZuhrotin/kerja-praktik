6f48a7e20e880a737ced3b574727188504903f66
Abil Zuhrotin <abilzuhrotins@gmail.com>
2026-04-16 19:33:53 +0700

    style: step-1 reservasi

---
+++ src/app/reservasi/page.js
@@ -0,0 +1,10 @@
+import StepOne from './component/step-1'; // Import komponennya
+
+export default function ReservasiPage() {
+  
+  return (
+    <main className="min-h-screen py-4 md:py-8">
+      <StepOne />
+    </main>
+  );
+}
